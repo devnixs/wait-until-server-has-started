2abd2ead76ded69933f65ac3c4db4397bf535090
Raphael ATALLAH <4487892+devnixs@users.noreply.github.com>
2019-07-01 17:41:53 +0200

    Update wait.sh

--- wait.sh
+++ wait.sh
@@ -4,8 +4,8 @@ counter=0
 until $(curl -X GET --output /dev/null --silent --head --fail $1); do
     printf '.'
     sleep 5
-    
-    if [ "$counter" -gt 120 ]; then
+    counter=$((counter+1))
+    if [ "$counter" -gt 100 ]; then
        echo "Waited for too long. Exiting."
        exit 1
     fi
